0121070ebf70ab2c834cec4db2c083def98d87a0
Joeke Kloosterman <joekekloosterman@gmail.com>
2016-01-07 19:30:38 +0100

    update minify cmp

---
+++ _build/gpm_resolvers/gpm.resolve.tables.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Resolve creating db tables
+ *
+ * THIS RESOLVER IS AUTOMATICALLY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package modxminify
+ * @subpackage build
+ */
+
+if ($object->xpdo) {
+    $modx =& $object->xpdo;
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            $modelPath = $modx->getOption('modxminify.core_path', null, $modx->getOption('core_path') . 'components/modxminify/') . 'model/';
+            $modx->addPackage('modxminify', $modelPath, 'modx_');
+
+            $manager = $modx->getManager();
+
+            $manager->createObjectContainer('modxMinifyGroup');
+            $manager->createObjectContainer('modxMinifyFile');
+
+            break;
+    }
+}
+
+return true;
